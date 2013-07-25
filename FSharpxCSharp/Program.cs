b2c9cda018facb1aa321048c79c24a2a46b8ffe3
mausch <mauricioscheffer@gmail.com>
2013-07-25 12:00:24 -0300

    Skeleton project using FSharpx in C#

---
+++ FSharpxCSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using FSharpx;
+using Microsoft.FSharp.Collections;
+
+namespace FSharpxCSharp {
+    internal class Program {
+        private static void Main(string[] args) {
+            FSharpList<int> a = FSharpList.Create(1, 2, 3);
+            Console.WriteLine(a);
+        }
+    }
+}
