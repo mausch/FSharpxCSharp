da2feacd6b5d1c04c4b5f77cea6a56b5db12888e
mausch <mauricioscheffer@gmail.com>
2013-07-25 12:05:20 -0300

    Add option

--- FSharpxCSharp/Program.cs
+++ FSharpxCSharp/Program.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using FSharpx;
 using Microsoft.FSharp.Collections;
+using Microsoft.FSharp.Core;
 
 namespace FSharpxCSharp {
     internal class Program {
         private static void Main(string[] args) {
             FSharpList<int> a = FSharpList.Create(1, 2, 3);
             Console.WriteLine(a);
+            FSharpOption<int> opt = 5.Some();
+            Console.WriteLine(opt);
         }
     }
 }
